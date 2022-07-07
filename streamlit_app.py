418123177a7b6b0d980c63e7f733072ffe43c3f8
silvi-1 <108897949+silvi-1@users.noreply.github.com>
2022-07-07 21:37:04 +0200

    Update streamlit_app.py
    breakfast menu corrected

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ streamlit.header('Breakfast')
 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled free-range egg')
+streamlit.text('Hard-Boiled Free-Range Egg')
